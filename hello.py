7e12c8528474d0ddd74611bd0e46ba9fa3848863
Kevin Lacker <lacker@gmail.com>
2024-11-03 09:42:59 -0800

    refactor to use an App

--- hello.py
+++ hello.py
@@ -4,20 +4,23 @@ import random
 SCREEN_SIZE = 200
 CELLS = 5
 CELL_SIZE = SCREEN_SIZE // CELLS
-pyxel.init(SCREEN_SIZE, SCREEN_SIZE)
 
 
-def update():
-    if pyxel.btnp(pyxel.KEY_Q):
-        pyxel.quit()
+class App:
+    def __init__(self):
+        pyxel.init(SCREEN_SIZE, SCREEN_SIZE)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)
+        for i in range(5):
+            for j in range(5):
+                color = random.randint(0, 15)
+                pyxel.rect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE, color)
 
 
-def draw():
-    pyxel.cls(0)
-    for i in range(5):
-        for j in range(5):
-            color = random.randint(0, 15)
-            pyxel.rect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE, color)
-
-
-pyxel.run(update, draw)
+App()
