12cbccd99c0d2c719ed2ceb0ebce0558ac6559a0
Kevin Lacker <lacker@gmail.com>
2024-11-09 06:39:18 -0800

    pixel based player

--- hello.py
+++ hello.py
@@ -1,17 +1,17 @@
 import pyxel
-import random
 
 SCREEN_SIZE = 200
-CELLS = 5
-CELL_SIZE = SCREEN_SIZE // CELLS
 PLAYER_COLOR = 8  # Define a constant color for the player
 BORDER_COLOR = 5  # 5 corresponds to a dark grey color
 
+
 class Player:
     def __init__(self, x, y, color):
+        "x and y are in pixels."
         self.x = x
         self.y = y
         self.color = color
+
 
 class App:
     def __init__(self):
@@ -24,23 +24,13 @@ class App:
             pyxel.quit()
         # Handle player movement
         if pyxel.btnp(pyxel.KEY_RIGHT):
-            self.player.x = min(self.player.x + 1, CELLS - 1)
+            self.player.x += 1
         if pyxel.btnp(pyxel.KEY_LEFT):
-            self.player.x = max(self.player.x - 1, 0)
-        if pyxel.btnp(pyxel.KEY_DOWN):
-            self.player.y = min(self.player.y + 1, CELLS - 1)
-        if pyxel.btnp(pyxel.KEY_UP):
-            self.player.y = max(self.player.y - 1, 0)
+            self.player.x -= 1
 
     def draw(self):
         pyxel.cls(0)
-        # Draw grid cells
-        for i in range(CELLS):
-            for j in range(CELLS):
-                if i == self.player.x and j == self.player.y:
-                    pyxel.rect(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE, self.player.color)
-                else:
-                    pyxel.rectb(i * CELL_SIZE, j * CELL_SIZE, CELL_SIZE, CELL_SIZE, BORDER_COLOR)  # Draw border
-                    pyxel.rect(i * CELL_SIZE + 1, j * CELL_SIZE + 1, CELL_SIZE - 2, CELL_SIZE - 2, 0)  # Fill cell with black
+        # TODO: draw player
+
 
 App()
