463a331aec7bfab8315915625f1141d5bfb2b11c
Kevin Lacker <lacker@gmail.com>
2024-11-10 16:54:02 -0800

    jumping square

--- hello.py
+++ hello.py
@@ -1,22 +1,20 @@
 import pyxel
 
 SCREEN_SIZE = 200
+PLAYER_SIZE = 10
+RUN_SPEED = 3
+JUMP_ACCEL = 10
+MAX_POS = SCREEN_SIZE - PLAYER_SIZE
 PLAYER_COLOR = 8  # Define a constant color for the player
-BORDER_COLOR = 5  # 5 corresponds to a dark grey color
-
-
-class Player:
-    def __init__(self, x, y, color):
-        "x and y are in pixels."
-        self.x = x
-        self.y = y
-        self.color = color
 
 
 class App:
     def __init__(self):
-        pyxel.init(SCREEN_SIZE, SCREEN_SIZE)
-        self.player = Player(0, 0, PLAYER_COLOR)  # Initialize player at top-left corner
+        pyxel.init(SCREEN_SIZE, SCREEN_SIZE, title="Gravity Test")
+        self.x = 0
+        self.y = MAX_POS
+        self.dx = 0
+        self.dy = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -24,13 +22,37 @@ class App:
             pyxel.quit()
         # Handle player movement
         if pyxel.btnp(pyxel.KEY_RIGHT):
-            self.player.x += 1
+            self.dx = RUN_SPEED
         if pyxel.btnp(pyxel.KEY_LEFT):
-            self.player.x -= 1
+            self.dx = -RUN_SPEED
+        if pyxel.btnr(pyxel.KEY_RIGHT) and self.dx > 0:
+            self.dx = 0
+        if pyxel.btnr(pyxel.KEY_LEFT) and self.dx < 0:
+            self.dx = 0
+        if pyxel.btnp(pyxel.KEY_UP) and self.y == MAX_POS:
+            self.dy = -JUMP_ACCEL
+
+        # Inertia
+        self.x += self.dx
+        self.y += self.dy
+
+        # Gravity
+        self.dy += 1
+
+        # Limits
+        if self.x < 0:
+            self.x = 0
+        elif self.x > MAX_POS:
+            self.x = MAX_POS
+        if self.y < 0:
+            self.y = 0
+        elif self.y > MAX_POS:
+            self.dy = 0
+            self.y = MAX_POS
 
     def draw(self):
         pyxel.cls(0)
-        # TODO: draw player
+        pyxel.rect(self.x, self.y, PLAYER_SIZE, PLAYER_SIZE, PLAYER_COLOR)
 
 
 App()
