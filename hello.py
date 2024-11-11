201dd064469446873a04b1af2d2e315d48ee8700
Kevin Lacker <lacker@gmail.com>
2024-11-11 10:22:04 -0800

    bullets

--- hello.py
+++ hello.py
@@ -4,6 +4,8 @@ SCREEN_SIZE = 200
 PLAYER_SIZE = 10
 RUN_SPEED = 3
 JUMP_ACCEL = 10
+BULLET_SPEED = 6
+BULLET_COLOR = 11
 MAX_POS = SCREEN_SIZE - PLAYER_SIZE
 PLAYER_COLOR = 8  # Define a constant color for the player
 
@@ -15,6 +17,7 @@ class App:
         self.y = MAX_POS
         self.dx = 0
         self.dy = 0
+        self.bullets = []
         pyxel.run(self.update, self.draw)
 
     def update(self):
@@ -32,9 +35,23 @@ class App:
         if pyxel.btnp(pyxel.KEY_UP) and self.y == MAX_POS:
             self.dy = -JUMP_ACCEL
 
+        # Spawn bullets
+        if pyxel.btnp(pyxel.KEY_SPACE):
+            direction = 1
+            if self.dx < 0:
+                direction = -1
+
+            self.bullets.append((self.x, self.y, direction))
+
         # Inertia
         self.x += self.dx
         self.y += self.dy
+        new_bullets = []
+        for x, y, direction in self.bullets:
+            if x < 0 or x > MAX_POS:
+                continue
+            new_bullets.append((x + BULLET_SPEED * direction, y, direction))
+        self.bullets = new_bullets
 
         # Gravity
         self.dy += 1
@@ -53,6 +70,8 @@ class App:
     def draw(self):
         pyxel.cls(0)
         pyxel.rect(self.x, self.y, PLAYER_SIZE, PLAYER_SIZE, PLAYER_COLOR)
+        for x, y, _ in self.bullets:
+            pyxel.rect(x, y, PLAYER_SIZE, PLAYER_SIZE, BULLET_COLOR)
 
 
 App()
