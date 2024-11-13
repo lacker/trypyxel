35269a8214d0ed5ffe7785a324df3047b2f71a3a
Kevin Lacker <lacker@gmail.com>
2024-11-12 16:25:15 -0800

    colors

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
+import random
 import pyxel
 
-SCREEN_SIZE = 200
+SCREEN_SIZE = 300
 PLAYER_SIZE = 10
 RUN_SPEED = 3
 JUMP_ACCEL = 10
 BULLET_SPEED = 6
-BULLET_COLOR = 11
+BULLET_COLORS = [1, 2, 3, 4, 5, 6, 11]
 MAX_POS = SCREEN_SIZE - PLAYER_SIZE
 PLAYER_COLOR = 8  # Define a constant color for the player
 
@@ -17,6 +18,7 @@ class App:
         self.y = MAX_POS
         self.dx = 0
         self.dy = 0
+        self.direction = 1
         self.bullets = []
         pyxel.run(self.update, self.draw)
 
@@ -26,31 +28,35 @@ class App:
         # Handle player movement
         if pyxel.btnp(pyxel.KEY_RIGHT):
             self.dx = RUN_SPEED
+            self.direction = 1
         if pyxel.btnp(pyxel.KEY_LEFT):
             self.dx = -RUN_SPEED
+            self.direction = -1
         if pyxel.btnr(pyxel.KEY_RIGHT) and self.dx > 0:
             self.dx = 0
         if pyxel.btnr(pyxel.KEY_LEFT) and self.dx < 0:
             self.dx = 0
-        if pyxel.btnp(pyxel.KEY_UP) and self.y == MAX_POS:
+        if pyxel.btnp(pyxel.KEY_UP):
+            # and self.y == MAX_POS:
             self.dy = -JUMP_ACCEL
 
         # Spawn bullets
         if pyxel.btnp(pyxel.KEY_SPACE):
-            direction = 1
-            if self.dx < 0:
-                direction = -1
-
-            self.bullets.append((self.x, self.y, direction))
+            self.bullets.append(
+                (self.x, self.y, self.direction, random.choice(BULLET_COLORS))
+            )
 
         # Inertia
         self.x += self.dx
         self.y += self.dy
         new_bullets = []
-        for x, y, direction in self.bullets:
-            if x < 0 or x > MAX_POS:
-                continue
-            new_bullets.append((x + BULLET_SPEED * direction, y, direction))
+        for x, y, direction, color in self.bullets:
+            if x < 0:
+                new_bullets.append((0, y, -direction, color))
+            elif x > MAX_POS:
+                new_bullets.append((MAX_POS, y, -direction, color))
+            else:
+                new_bullets.append((x + BULLET_SPEED * direction, y, direction, color))
         self.bullets = new_bullets
 
         # Gravity
@@ -70,8 +76,14 @@ class App:
     def draw(self):
         pyxel.cls(0)
         pyxel.rect(self.x, self.y, PLAYER_SIZE, PLAYER_SIZE, PLAYER_COLOR)
-        for x, y, _ in self.bullets:
-            pyxel.rect(x, y, PLAYER_SIZE, PLAYER_SIZE, BULLET_COLOR)
+        if self.direction < 0:
+            pyxel.rect(self.x, self.y, PLAYER_SIZE / 2, PLAYER_SIZE / 2, 10)
+        else:
+            pyxel.rect(
+                self.x + PLAYER_SIZE / 2, self.y, PLAYER_SIZE / 2, PLAYER_SIZE / 2, 10
+            )
+        for x, y, _, color in self.bullets:
+            pyxel.rect(x, y, PLAYER_SIZE, PLAYER_SIZE, color)
 
 
 App()
