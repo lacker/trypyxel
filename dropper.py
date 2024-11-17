b9903784bbacb64c04db413c6a5d3ef0f19ebf2c
Kevin Lacker <lacker@gmail.com>
2024-11-17 08:00:18 -0800

    left and right

--- dropper.py
+++ dropper.py
@@ -4,12 +4,14 @@ import pyxel
 SCREEN_SIZE = 300
 THING_SIZE = 10
 MAX_POS = SCREEN_SIZE - THING_SIZE
+H_SPEED = 3
 
 
 class Thing:
     def __init__(self):
         self.x = random.randint(0, MAX_POS)
         self.y = 0
+        self.dx = 0
 
 
 class App:
@@ -22,6 +24,20 @@ class App:
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
+        if pyxel.btnp(pyxel.KEY_RIGHT):
+            self.active.dx = H_SPEED
+        if pyxel.btnp(pyxel.KEY_LEFT):
+            self.active.dx = -H_SPEED
+        if pyxel.btnr(pyxel.KEY_RIGHT) and self.active.dx > 0:
+            self.active.dx = 0
+        if pyxel.btnr(pyxel.KEY_LEFT) and self.active.dx < 0:
+            self.active.dx = 0
+
+        self.active.x += self.active.dx
+        if self.active.x < 0:
+            self.active.x = 0
+        elif self.active.x > MAX_POS:
+            self.active.x = MAX_POS
 
         if self.active.y >= MAX_POS:
             self.things.append(self.active)
