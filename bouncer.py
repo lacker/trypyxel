6067af3847cbecf24870ae62b198aef156fb714a
Kevin Lacker <lacker@gmail.com>
2024-11-25 09:36:55 -0800

    use explicit vector

--- bouncer.py
+++ bouncer.py
@@ -8,44 +8,58 @@ MAX_POS = SCREEN_SIZE - THING_SIZE
 SPEED = 6
 
 
+class Vector:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector(self.x + other.x, self.y + other.y)
+
+
+def random_vector(size):
+    angle = random.random() * 100
+    x = math.cos(angle) * size
+    y = math.sin(angle) * size
+    return Vector(x, y)
+
+
 class App:
     def __init__(self):
         pyxel.init(SCREEN_SIZE, SCREEN_SIZE, title="Thing Bouncer")
-        self.x = random.randrange(0, MAX_POS)
-        self.y = random.randrange(0, MAX_POS)
-        self.randomize_direction()
+        x = random.randrange(0, MAX_POS)
+        y = random.randrange(0, MAX_POS)
+        self.position = Vector(x, y)
+        self.velocity = random_vector(SPEED)
         pyxel.run(self.update, self.draw)
-
-    def randomize_direction(self):
-        angle = random.random() * 100
-        self.dx = math.cos(angle) * SPEED
-        self.dy = math.sin(angle) * SPEED
 
     def update(self):
         if pyxel.btnp(pyxel.KEY_Q):
             pyxel.quit()
         if pyxel.btnp(pyxel.KEY_SPACE):
-            self.randomize_direction()
+            self.velocity = random_vector(SPEED)
 
-        self.x += self.dx
-        if self.x <= 0 and self.dx < 0:
-            self.x = 0
-            self.dx = -self.dx
-        if self.x >= MAX_POS and self.dx > 0:
-            self.x = MAX_POS
-            self.dx = -self.dx
+        self.position += self.velocity
 
-        self.y += self.dy
-        if self.y <= 0 and self.dy < 0:
-            self.y = 0
-            self.dy = -self.dy
-        if self.y >= MAX_POS and self.dy > 0:
-            self.y = MAX_POS
-            self.dy = -self.dy
+        # Handle bouncing
+
+        if self.position.x <= 0 and self.velocity.x < 0:
+            self.position.x = 0
+            self.velocity.x = -self.velocity.x
+        if self.position.x >= MAX_POS and self.velocity.x > 0:
+            self.position.x = MAX_POS
+            self.velocity.x = -self.velocity.x
+
+        if self.position.y <= 0 and self.velocity.y < 0:
+            self.position.y = 0
+            self.velocity.y = -self.velocity.y
+        if self.position.y >= MAX_POS and self.velocity.y > 0:
+            self.position.y = MAX_POS
+            self.velocity.y = -self.velocity.y
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, self.y, THING_SIZE, THING_SIZE, 8)
+        pyxel.rect(self.position.x, self.position.y, THING_SIZE, THING_SIZE, 8)
 
 
 App()
