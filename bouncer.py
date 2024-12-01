c2e476081b94c5ba8d168c3d6729212f476e46f1
Kevin Lacker <lacker@gmail.com>
2024-12-01 09:21:56 -0800

    difficulty increases

--- bouncer.py
+++ bouncer.py
@@ -5,7 +5,8 @@ import pyxel
 SCREEN_SIZE = 200
 THING_SIZE = 10
 MAX_POS = SCREEN_SIZE - THING_SIZE
-SPEED = 2
+BASE_SPEED = 2
+SPEED_BOOST = 0.05
 TURN = 2 * math.pi / 16
 INITIAL_COLOR = 8
 LAST_COLOR = 15
@@ -18,6 +19,9 @@ class Vector:
 
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
+
+    def __mul__(self, k):
+        return Vector(self.x * k, self.y * k)
 
 
 def random_vector(size):
@@ -38,12 +42,15 @@ class App:
         y = random.randrange(0, MAX_POS)
         self.direction = 0
         self.position = Vector(x, y)
-        self.velocity = polar(self.direction, SPEED)
+        self.velocity = polar(self.direction, BASE_SPEED)
         self.color = INITIAL_COLOR
         self.score = 0
         pyxel.run(self.update, self.draw)
 
-    def change_color(self):
+    def bounce(self):
+        """
+        Called when the thing bounces off a wall.
+        """
         self.color += 1
         if self.color > LAST_COLOR:
             self.color = INITIAL_COLOR
@@ -55,7 +62,8 @@ class App:
         if pyxel.btnp(pyxel.KEY_SPACE):
             self.score += 1
             self.direction += TURN
-            self.velocity = polar(self.direction, SPEED)
+            speed = BASE_SPEED + self.score * SPEED_BOOST
+            self.velocity = polar(self.direction, speed)
 
         self.position += self.velocity
 
@@ -64,20 +72,20 @@ class App:
         if self.position.x <= 0 and self.velocity.x < 0:
             self.position.x = 0
             self.velocity.x = -self.velocity.x
-            self.change_color()
+            self.bounce()
         if self.position.x >= MAX_POS and self.velocity.x > 0:
             self.position.x = MAX_POS
             self.velocity.x = -self.velocity.x
-            self.change_color()
+            self.bounce()
 
         if self.position.y <= 0 and self.velocity.y < 0:
             self.position.y = 0
             self.velocity.y = -self.velocity.y
-            self.change_color()
+            self.bounce()
         if self.position.y >= MAX_POS and self.velocity.y > 0:
             self.position.y = MAX_POS
             self.velocity.y = -self.velocity.y
-            self.change_color()
+            self.bounce()
 
     def draw(self):
         pyxel.cls(0)
