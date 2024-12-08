4bb29601b5cde31df3da1535e99c6a3296498889
Kevin Lacker <lacker@gmail.com>
2024-12-08 10:28:09 -0800

    dropping every 30 ticks

--- doctor.py
+++ doctor.py
@@ -4,20 +4,32 @@ import pyxel
 BOARD_HEIGHT = 14
 BOARD_WIDTH = 8
 CELL_SIZE = 10
+DROP_TICKS = 30
 COLORS = [12, 10, 8]
 
 
 class App:
     def __init__(self):
         pyxel.init(CELL_SIZE * BOARD_WIDTH, CELL_SIZE * BOARD_HEIGHT, title="Doctor M")
+        self.ticks = 0
+        self.active_x = BOARD_WIDTH // 2
+        self.active_y = 0
+        self.active_color = random.choice(COLORS)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        pass
+        self.ticks += 1
+        if self.ticks % DROP_TICKS == 0:
+            self.active_y += 1
+            if self.active_y == BOARD_HEIGHT:
+                self.active_y = 0
+                self.active_color = random.choice(COLORS)
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(0, 0, CELL_SIZE, CELL_SIZE, random.choice(COLORS))
+        left = self.active_x * CELL_SIZE
+        top = self.active_y * CELL_SIZE
+        pyxel.rect(left, top, CELL_SIZE, CELL_SIZE, self.active_color)
 
 
 App()
